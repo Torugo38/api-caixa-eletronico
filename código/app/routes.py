db1d381fe8e39ca0838a368a0fcf765a94c71795
Victor Hugo <90855346+Torugo38@users.noreply.github.com>
2024-07-01 22:48:49 -0300

    Update routes

--- código/app/routes.py
+++ código/app/routes.py
@@ -4,10 +4,17 @@ bp = Blueprint('routes', __name__)
 
 def calcular_cedulas(valor):
     cedulas = [100, 50, 20, 10, 5, 2]
-    resultado = {}
+    resultado = {cedula: 0 for cedula in cedulas}
     for cedula in cedulas:
-        quantidade, valor = divmod(valor, cedula)
-        resultado[cedula] = quantidade
+        if valor >= cedula:
+            quantidade, valor = divmod(valor, cedula)
+            resultado[cedula] = quantidade
+
+    if valor == 1 and resultado[5] > 0:
+        resultado[5] -= 1
+        resultado[2] += 3
+        valor -= 1
+
     return resultado
 
 @bp.route('/api/saque', methods=['POST'])
